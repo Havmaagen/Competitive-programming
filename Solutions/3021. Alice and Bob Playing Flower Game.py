a2f9602c501b0419f9f35a10b118fb104cc0224a
Havmaagen <98764246+Havmaagen@users.noreply.github.com>
2025-08-29 22:19:04 +0200

    Update

---
+++ Solutions/3021. Alice and Bob Playing Flower Game.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def flowerGame(self, n, m):
+        """
+        :type n: int
+        :type m: int
+        :rtype: int
+        """
+        
+        n_pairs = (m * n) // 2
+        
+        return n_pairs
